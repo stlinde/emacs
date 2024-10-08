283f2a01e31a51afe2274429e1fa0c70a893df4e
Sebastian Hempel Linde <stl@yco.dk>
2024-10-08 15:17:49 +0200

    feat: treesitter

--- init.el
+++ init.el
@@ -70,6 +70,8 @@
 (add-hook 'org-mode-hook #'display-line-numbers-mode)
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 
+
+
 (setopt display-line-numbers-width 3
         display-line-numbers-type 'relative)
 (when (boundp 'display-fill-column-indicator)
@@ -203,20 +205,60 @@
 
   (global-set-key (kbd "C-c n") 'obsidian-hydra/body))
 
-(setq treesit-language-source-alist
-      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-        (python "https://github.com/tree-sitter/tree-sitter-python")
-        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-        (toml "https://github.com/tree-sitter/tree-sitter-toml")
-        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+(require 'treesit)
+(when (treesit-available-p)
+  (setq treesit-language-source-alist
+        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+          (python "https://github.com/tree-sitter/tree-sitter-python")
+          (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 
-(mapc #'treesit-install-language-grammar
-      (mapcar #'car treesit-language-source-alist))q
+  (mapc #'treesit-install-language-grammar
+        (mapcar #'car treesit-language-source-alist))
 
-(setq major-mode-ramap-alist
-      '((yaml-mode . yaml-ts-mode)
-        (bash-mode . bash-ts-mode)
-        (python-mode . python-ts-mode)))
+  (setq major-mode-ramap-alist
+        '((yaml-mode . yaml-ts-mode)
+          (toml-mode . toml-ts-mode)
+          (bash-mode . bash-ts-mode)
+          (python-mode . python-ts-mode))))
+
+
+(require 'cl-lib)
+(defun shl/git-clone-clipboard-url ()
+  "Clone git URL in clipboard asynchronously and open in dired when finished."
+  (interactive)
+  (cl-assert (string-match-p "^\\(http\\|https\\|ssh\\)://" (current-kill 0)) nil "No URL in clipboard")
+  (let* ((url (current-kill 0))
+         (download-dir (expand-file-name "~/data/resources/code-reference/"))
+         (project-dir (concat (file-name-as-directory download-dir)
+                              (file-name-base url)))
+         (default-directory download-dir)
+         (command (format "git clone %s" url))
+         (buffer (generate-new-buffer (format "*%s*" command)))
+         (proc))
+    (when (file-exists-p project-dir)
+      (if (y-or-n-p (format "%s exists. delete?" (file-name-base url)))
+          (delete-directory project-dir t)
+        (user-error "Bailed")))
+    (switch-to-buffer buffer)
+    (setq proc (start-process-shell-command (nth 0 (split-string command)) buffer command))
+    (with-current-buffer buffer
+      (setq default-directory download-dir)
+      (shell-command-save-pos-or-erase)
+      (require 'shell)
+      (shell-mode)
+      (view-mode +1))
+    (set-process-sentinel proc (lambda (process state)
+                                 (let ((output (with-current-buffer (process-buffer process)
+                                                 (buffer-string))))
+                                   (kill-buffer (process-buffer process))
+                                   (if (= (process-exit-status process) 0)
+                                       (progn
+                                         (message "finished: %s" command)
+                                         (dired project-dir))
+                                     (user-error (format "%s\n%s" command output))))))
+    (set-process-filter proc #'comint-output-filter)))
 
 
 ;;; init.el ends here
